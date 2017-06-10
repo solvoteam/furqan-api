7214847c549e0d35266d61a47dfae07c495e29e1
mik <mikail.aziev@gmail.com>
2017-06-10 17:58:47 +0300

    disable timestamps on models

--- app/Models/Ayat.php
+++ app/Models/Ayat.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ayat extends Model
 {
-    //
+    public $timestamps = false;
 }
--- app/Models/Translation.php
+++ app/Models/Translation.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Translation extends Model
 {
-    //
+    public $timestamps = false;
 }
