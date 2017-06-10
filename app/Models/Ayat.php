14e6bfef156c22a1aae376fd8c3320a62207a86f
mik <mikail.aziev@gmail.com>
2017-06-10 18:23:57 +0300

    create Ayats controller and routes

--- app/Models/Ayat.php
+++ app/Models/Ayat.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Ayat extends Model
 {
     public $timestamps = false;
+
+    public function translations()
+    {
+        return $this->hasMany(AyatTranslation::class);
+    }
+
+    public function translation($translation_id)
+    {
+        return $this->translations()->whereId($translation_id)->firstOrFail();
+    }
 }
