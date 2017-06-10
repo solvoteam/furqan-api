cd338c1df1e98818199d78932a6a276a6488b5cb
mik <mikail.aziev@gmail.com>
2017-06-10 12:45:53 +0300

    create Models directory

--- app/Models/Ayat.php
+++ app/Models/Ayat.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Translation extends Model
+class Ayat extends Model
 {
     //
 }
--- app/Models/Translation.php
+++ app/Models/Translation.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class AyatText extends Model
+class Translation extends Model
 {
     //
 }
